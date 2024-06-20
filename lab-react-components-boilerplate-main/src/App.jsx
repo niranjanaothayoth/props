3b127996b5905cb8c8de768f2472136a31740ee5
niranjanaothayoth <160855491+niranjanaothayoth@users.noreply.github.com>
2024-06-20 21:29:02 +0530

    Add files via upload

---
+++ lab-react-components-boilerplate-main/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Body from './components/Body';
+import GallaryFooter from './components/GallaryFooter';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <GallaryFooter />
+    </div>
+  );
+}
+
+export default App;
